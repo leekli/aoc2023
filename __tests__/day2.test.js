b000731b3d49d5791bf778c6b3463b8d018dd6c2
leekli <li.kirkham@live.com>
2023-12-02 21:40:53 +0000

    solves day 2 part 2

--- __tests__/day2.test.js
+++ __tests__/day2.test.js
@@ -1,4 +1,4 @@
-const { part1 } = require('../day2/day2');
+const { part1, part2 } = require('../day2/day2');
 const mainDataInput = require('../day2/day2-input');
 
 describe('AOC 2023 Day 2', () => {
@@ -42,9 +42,38 @@ describe('AOC 2023 Day 2', () => {
 
       expect(part1(input)).toBe(8);
     });
-    test('should test with the day 2 AOC 2023 input file', () => {
+    test('should test part 1 with the day 2 AOC 2023 input file', () => {
       expect(part1(mainDataInput)).toBe(2156);
     });
   });
-  describe('Part 2', () => {});
+  describe('Part 2', () => {
+    test('When given a single game, should return the power of a set of cubes multipled together which were the fewest possible set of cubes which could have been played with', () => {
+      let input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green`;
+      expect(part2(input)).toBe(48);
+
+      input = `Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue`;
+      expect(part2(input)).toBe(12);
+
+      input = `Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red`;
+      expect(part2(input)).toBe(1560);
+
+      input = `Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red`;
+      expect(part2(input)).toBe(630);
+
+      input = `Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+      expect(part2(input)).toBe(36);
+    });
+    test('When given multiple games, should return the sum of all games', () => {
+      const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+
+      expect(part2(input)).toBe(2286);
+    });
+    test('should test part 2 with the day 2 AOC 2023 input file', () => {
+      expect(part2(mainDataInput)).toBe(66909);
+    });
+  });
 });
--- day2/day2.js
+++ day2/day2.js
@@ -35,4 +35,61 @@ const part1 = (input) => {
   return total;
 };
 
-module.exports = { part1 };
+const part2 = (input) => {
+  // Parse the input
+  const gamesSplit = input.split('\n').map((line) => line.trim());
+
+  let total = 0;
+
+  // Work through each Game Id individually
+  gamesSplit.forEach((game) => {
+    const parsedInputGameIdRemoved = game.replace(/Game (\d+): /g, '');
+    const subGamesSplit = parsedInputGameIdRemoved.split('; ');
+
+    // Tracker to track highest colour of each cube
+    const currGameColourTracker = {
+      red: 0,
+      blue: 0,
+      green: 0,
+    };
+
+    // Work through each sub game (pick of the bag) and track the red/green/blue cubes for highest number which means the lowest number of cubes needed for that game
+    subGamesSplit.forEach((currSubGame) => {
+      const splitSubGameColours = currSubGame.split(', ');
+
+      splitSubGameColours.forEach((currSubGameColour) => {
+        const [count, colour] = currSubGameColour.split(' ');
+        const countAsNum = Number(count);
+
+        if (colour === 'red') {
+          if (countAsNum > currGameColourTracker['red']) {
+            currGameColourTracker['red'] = countAsNum;
+          }
+        }
+
+        if (colour === 'blue') {
+          if (countAsNum > currGameColourTracker['blue']) {
+            currGameColourTracker['blue'] = countAsNum;
+          }
+        }
+
+        if (colour === 'green') {
+          if (countAsNum > currGameColourTracker['green']) {
+            currGameColourTracker['green'] = countAsNum;
+          }
+        }
+      });
+    });
+
+    const powerOfCubes =
+      currGameColourTracker['red'] *
+      currGameColourTracker['blue'] *
+      currGameColourTracker['green'];
+
+    total += powerOfCubes;
+  });
+
+  return total;
+};
+
+module.exports = { part1, part2 };
