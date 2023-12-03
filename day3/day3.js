c381b1e90cc62c079e1be0bcde1075fa0b441a5f
leekli <li.kirkham@live.com>
2023-12-03 19:31:23 +0000

    slight refactor to day 3 parts 1 and 2

--- day3/day3.js
+++ day3/day3.js
@@ -3,45 +3,46 @@ const part1 = (input) => {
 
   const splitLines = input.split('\n').map((line) => line.trim());
 
-  const numbersList = buildNumsArr(splitLines);
-  const symbolsList = buildSymbolsArr(splitLines);
+  const { numbersList, symbolsList } = buildNumsAndSymbolsLists(splitLines);
 
   for (let i = 0; i < numbersList.length; i++) {
-    // for each number in an individual row
     for (let j = 0; j < symbolsList.length; j++) {
-      // check if a symbol is to the right
-      if (symbolsList[j].row === numbersList[i].row) {
-        if (symbolsList[j].index === numbersList[i].endIndex) {
-          total += numbersList[i].num;
-          numbersList[i] = { numChecked: true };
-        }
+      // check if a symbol is to the right 👉
+      if (
+        symbolsList[j].row === numbersList[i].row &&
+        symbolsList[j].index === numbersList[i].endIndex
+      ) {
+        total += numbersList[i].num;
+        numbersList[i] = { numChecked: true };
       }
-      // check if a symbol is to the left
-      if (symbolsList[j].row === numbersList[i].row) {
-        if (symbolsList[j].index + 1 === numbersList[i].startIndex) {
-          total += numbersList[i].num;
-          numbersList[i] = { numChecked: true };
-        }
+
+      // check if a symbol is to the left 👈
+      if (
+        symbolsList[j].row === numbersList[i].row &&
+        symbolsList[j].index + 1 === numbersList[i].startIndex
+      ) {
+        total += numbersList[i].num;
+        numbersList[i] = { numChecked: true };
       }
-      // check if a symbol is to below the number & diagonal bottom left or right
-      if (symbolsList[j].row - 1 === numbersList[i].row) {
-        if (
-          symbolsList[j].index >= numbersList[i].startIndex - 1 &&
-          symbolsList[j].index <= numbersList[i].endIndex
-        ) {
-          total += numbersList[i].num;
-          numbersList[i] = { numChecked: true };
-        }
+
+      // check if a symbol is to below the number & diagonal bottom left or right 👇
+      if (
+        symbolsList[j].row - 1 === numbersList[i].row &&
+        symbolsList[j].index >= numbersList[i].startIndex - 1 &&
+        symbolsList[j].index <= numbersList[i].endIndex
+      ) {
+        total += numbersList[i].num;
+        numbersList[i] = { numChecked: true };
       }
-      // check if a symbol is to above the number & diagonal top left or right
-      if (symbolsList[j].row === numbersList[i].row - 1) {
-        if (
-          symbolsList[j].index >= numbersList[i].startIndex - 1 &&
-          symbolsList[j].index <= numbersList[i].endIndex
-        ) {
-          total += numbersList[i].num;
-          numbersList[i] = { numChecked: true };
-        }
+
+      // check if a symbol is to above the number & diagonal top left or right ☝️
+      if (
+        symbolsList[j].row === numbersList[i].row - 1 &&
+        symbolsList[j].index >= numbersList[i].startIndex - 1 &&
+        symbolsList[j].index <= numbersList[i].endIndex
+      ) {
+        total += numbersList[i].num;
+        numbersList[i] = { numChecked: true };
       }
     }
   }
@@ -54,47 +55,50 @@ const part2 = (input) => {
 
   const splitLines = input.split('\n').map((line) => line.trim());
 
-  const numbersList = buildNumsArr(splitLines);
-  const onlyGearsymbolsList = buildSymbolsArr(splitLines).filter(
+  const { numbersList, symbolsList } = buildNumsAndSymbolsLists(splitLines);
+
+  const onlyGearsymbolsList = symbolsList.filter(
     (symbol) => symbol.symbol === '*'
   );
 
   for (let i = 0; i < numbersList.length; i++) {
-    // for each number in an individual row
     for (let j = 0; j < onlyGearsymbolsList.length; j++) {
-      // check if a symbol is to the right
-      if (onlyGearsymbolsList[j].row === numbersList[i].row) {
-        if (onlyGearsymbolsList[j].index === numbersList[i].endIndex) {
-          onlyGearsymbolsList[j].numsAdjacentTo.push(numbersList[i].num);
-          numbersList[i] = { numChecked: true };
-        }
+      // check if a symbol is to the right 👉
+      if (
+        onlyGearsymbolsList[j].row === numbersList[i].row &&
+        onlyGearsymbolsList[j].index === numbersList[i].endIndex
+      ) {
+        onlyGearsymbolsList[j].numsAdjacentTo.push(numbersList[i].num);
+        numbersList[i] = { numChecked: true };
       }
-      // check if a symbol is to the left
-      if (onlyGearsymbolsList[j].row === numbersList[i].row) {
-        if (onlyGearsymbolsList[j].index + 1 === numbersList[i].startIndex) {
-          onlyGearsymbolsList[j].numsAdjacentTo.push(numbersList[i].num);
-          numbersList[i] = { numChecked: true };
-        }
+
+      // check if a symbol is to the left 👈
+      if (
+        onlyGearsymbolsList[j].row === numbersList[i].row &&
+        onlyGearsymbolsList[j].index + 1 === numbersList[i].startIndex
+      ) {
+        onlyGearsymbolsList[j].numsAdjacentTo.push(numbersList[i].num);
+        numbersList[i] = { numChecked: true };
       }
-      // check if a symbol is to below the number & diagonal bottom left or right
-      if (onlyGearsymbolsList[j].row - 1 === numbersList[i].row) {
-        if (
-          onlyGearsymbolsList[j].index >= numbersList[i].startIndex - 1 &&
-          onlyGearsymbolsList[j].index <= numbersList[i].endIndex
-        ) {
-          onlyGearsymbolsList[j].numsAdjacentTo.push(numbersList[i].num);
-          numbersList[i] = { numChecked: true };
-        }
+
+      // check if a symbol is to below the number & diagonal bottom left or right 👇
+      if (
+        onlyGearsymbolsList[j].row - 1 === numbersList[i].row &&
+        onlyGearsymbolsList[j].index >= numbersList[i].startIndex - 1 &&
+        onlyGearsymbolsList[j].index <= numbersList[i].endIndex
+      ) {
+        onlyGearsymbolsList[j].numsAdjacentTo.push(numbersList[i].num);
+        numbersList[i] = { numChecked: true };
       }
-      // check if a symbol is to above the number & diagonal top left or right
-      if (onlyGearsymbolsList[j].row === numbersList[i].row - 1) {
-        if (
-          onlyGearsymbolsList[j].index >= numbersList[i].startIndex - 1 &&
-          onlyGearsymbolsList[j].index <= numbersList[i].endIndex
-        ) {
-          onlyGearsymbolsList[j].numsAdjacentTo.push(numbersList[i].num);
-          numbersList[i] = { numChecked: true };
-        }
+
+      // check if a symbol is to above the number & diagonal top left or right ☝️
+      if (
+        onlyGearsymbolsList[j].row === numbersList[i].row - 1 &&
+        onlyGearsymbolsList[j].index >= numbersList[i].startIndex - 1 &&
+        onlyGearsymbolsList[j].index <= numbersList[i].endIndex
+      ) {
+        onlyGearsymbolsList[j].numsAdjacentTo.push(numbersList[i].num);
+        numbersList[i] = { numChecked: true };
       }
     }
   }
@@ -102,42 +106,30 @@ const part2 = (input) => {
   for (let i = 0; i < onlyGearsymbolsList.length; i++) {
     let gearRatio = 1;
 
-    onlyGearsymbolsList[i].numsAdjacentTo.forEach((num) => {
-      gearRatio *= num;
-    });
+    onlyGearsymbolsList[i].numsAdjacentTo.forEach((num) => (gearRatio *= num));
 
-    if (onlyGearsymbolsList[i].numsAdjacentTo.length >= 2) {
-      total += gearRatio;
-    }
+    if (onlyGearsymbolsList[i].numsAdjacentTo.length >= 2) total += gearRatio;
   }
 
   return total;
 };
 
-const buildNumsArr = (lines) => {
-  const numsList = [];
+const buildNumsAndSymbolsLists = (lines) => {
+  const numbersList = [];
+  const symbolsList = [];
 
   lines.forEach((row, index) => {
     const allFoundNumbers = [...row.matchAll(/\d+/dg)];
+    const allFoundSymbols = [...row.matchAll(/[^\d|.]/dg)];
 
     allFoundNumbers.forEach((num) => {
-      numsList.push({
+      numbersList.push({
         num: Number(num[0]),
         row: index,
         startIndex: num.indices[0][0],
         endIndex: num.indices[0][1],
       });
     });
-  });
-
-  return numsList;
-};
-
-const buildSymbolsArr = (lines) => {
-  const symbolsList = [];
-
-  lines.forEach((row, index) => {
-    const allFoundSymbols = [...row.matchAll(/[^\d|.|\s]/dg)];
 
     allFoundSymbols.forEach((symbol) => {
       symbolsList.push({
@@ -149,7 +141,7 @@ const buildSymbolsArr = (lines) => {
     });
   });
 
-  return symbolsList;
+  return { numbersList, symbolsList };
 };
 
 module.exports = { part1, part2 };
