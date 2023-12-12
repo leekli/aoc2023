4bbb5aa41fa39ad4b4b2fce91af2f232da85ff59
leekli <li.kirkham@live.com>
2023-12-12 21:25:13 +0000

    UNSOLVED: day 7 part 2

--- __tests__/day7.test.js
+++ __tests__/day7.test.js
@@ -3,6 +3,11 @@ const {
   convertHandToNums,
   convertHandToType,
 } = require('../day7/day7.js');
+const {
+  part2,
+  convertHandToNumsP2,
+  convertHandToTypeP2,
+} = require('../day7/day7-p2.js');
 const mainDataInput = require('../day7/day7-input.js');
 
 describe('AOC 2023 Day 7', () => {
@@ -123,5 +128,156 @@ describe('AOC 2023 Day 7', () => {
       expect(part1(mainDataInput)).toBe(251545216);
     });
   });
-  describe('Part 2', () => {});
+  describe('Part 2', () => {
+    test('util convertHandToTypeP2: should return an empty string and 0 rank when given empty input', () => {
+      const { handType, typeRank } = convertHandToTypeP2(``);
+      expect(handType).toBe('');
+      expect(typeRank).toBe(0);
+    });
+    test('util convertHandToTypeP2: should convert a single hand to a type and rank number for 5 of a kind', () => {
+      const { handType, typeRank } = convertHandToTypeP2(`AAAAA`);
+      expect(handType).toBe('5aK');
+      expect(typeRank).toBe(7);
+    });
+    test('util convertHandToTypeP2: should convert a single hand to a type and rank number for 4 of a kind', () => {
+      const { handType, typeRank } = convertHandToTypeP2(`AA8AA`);
+      expect(handType).toBe('4aK');
+      expect(typeRank).toBe(6);
+    });
+    test('util convertHandToTypeP2: should convert a single hand to a type and rank number for full house', () => {
+      const { handType, typeRank } = convertHandToTypeP2(`23332`);
+      expect(handType).toBe('Fh');
+      expect(typeRank).toBe(5);
+    });
+    test('util convertHandToTypeP2: should convert a single hand to a type and rank number for 3 of a kind', () => {
+      const { handType, typeRank } = convertHandToTypeP2(`TTT98`);
+      expect(handType).toBe('3aK');
+      expect(typeRank).toBe(4);
+    });
+    test('util convertHandToTypeP2: should convert a single hand to a type and rank number for two pair', () => {
+      const { handType, typeRank } = convertHandToTypeP2(`23432`);
+      expect(handType).toBe('2p');
+      expect(typeRank).toBe(3);
+    });
+    test('util convertHandToTypeP2: should convert a single hand to a type and rank number for one pair', () => {
+      const { handType, typeRank } = convertHandToType(`A23A4`);
+      expect(handType).toBe('1p');
+      expect(typeRank).toBe(2);
+    });
+    test('util convertHandToTypeP2: should convert a single hand to a type and rank number for high card', () => {
+      const { handType, typeRank } = convertHandToTypeP2(`23456`);
+      expect(handType).toBe('Hc');
+      expect(typeRank).toBe(1);
+    });
+    test('util convertHandToTypeP2: should convert a hand to the strongest possible type if it contains a `J` card', () => {
+      // Original five of a kind with 5 J cards --> stays 5 of a kind
+      let hand = convertHandToTypeP2(`JJJJJ`);
+      expect(hand.handType).toBe('5aK');
+      expect(hand.typeRank).toBe(7);
+
+      // Original four of a kind with 1 J card --> becomes 5 of a kind
+      hand = convertHandToTypeP2(`KKKJK`);
+      expect(hand.handType).toBe('5aK');
+      expect(hand.typeRank).toBe(7);
+
+      // Original full house with 2 J cards --> becomes 5 of a kind
+      hand = convertHandToTypeP2(`KKKJJ`);
+      expect(hand.handType).toBe('5aK');
+      expect(hand.typeRank).toBe(7);
+
+      // Original 3 of a kind given 1 J card --> becomes 4 of a kind
+      hand = convertHandToTypeP2(`TTT9J`);
+      expect(hand.handType).toBe('4aK');
+      expect(hand.typeRank).toBe(6);
+
+      // Original 3 of a kind given 2 J cards --> becomes 5 of a kind
+      hand = convertHandToTypeP2(`TTTJJ`);
+      expect(hand.handType).toBe('5aK');
+      expect(hand.typeRank).toBe(7);
+
+      // Original 2 pair given 1 J card --> becomes a full house
+      hand = convertHandToTypeP2(`23J32`);
+      expect(hand.handType).toBe('Fh');
+      expect(hand.typeRank).toBe(5);
+
+      // Original 2 pair given 2 J cards --> becomes 4 of a kind
+      hand = convertHandToTypeP2(`2J4J2`);
+      expect(hand.handType).toBe('4aK');
+      expect(hand.typeRank).toBe(6);
+
+      // Original 2 pair given 3 J cards --> becomes 5 of a kind
+      hand = convertHandToTypeP2(`2JJJ2`);
+      expect(hand.handType).toBe('5aK');
+      expect(hand.typeRank).toBe(7);
+
+      // Original 1 pair given 1 J card --> becomes 3 of a kind
+      hand = convertHandToTypeP2(`A2JA4`);
+      expect(hand.handType).toBe('3aK');
+      expect(hand.typeRank).toBe(4);
+
+      // Original 1 pair given 2 J cards --> becomes 3 of a kind
+      hand = convertHandToTypeP2(`AJ3AJ`);
+      expect(hand.handType).toBe('4aK');
+      expect(hand.typeRank).toBe(6);
+
+      // Original 1 pair given 2 J cards --> becomes 3 of a kind
+      hand = convertHandToTypeP2(`AJJAJ`);
+      expect(hand.handType).toBe('5aK');
+      expect(hand.typeRank).toBe(7);
+    });
+    test('util convertHandToNumsP2: should return empty array when given empty input', () => {
+      const testHand = ``;
+      expect(convertHandToNumsP2(testHand)).toEqual([]);
+    });
+    test('util convertHandToNumsP2: should convert a single hand to numbers', () => {
+      let testHand = `32T3K`;
+      expect(convertHandToNumsP2(testHand)).toEqual([3, 2, 10, 3, 12]);
+
+      testHand = `KK677`;
+      expect(convertHandToNumsP2(testHand)).toEqual([12, 12, 6, 7, 7]);
+
+      testHand = `T55J5`;
+      expect(convertHandToNumsP2(testHand)).toEqual([10, 5, 5, 1, 5]);
+    });
+    test('should return a number', () => {
+      expect(typeof part2('')).toBe('number');
+    });
+    test('should return 0 when given an empty input', () => {
+      expect(part2('')).toBe(0);
+    });
+    test('should return the total winnings for a single hand and single bet', () => {
+      const input = `32T3K 765`;
+      expect(part2(input)).toBe(765);
+    });
+    test('should return the total winnings for a multiple hands with distinct hand types', () => {
+      let input = `32T3K 765
+        T55J5 684`;
+      expect(part2(input)).toBe(2133);
+
+      input = `32T3K 765
+      T55J5 684
+      AAAAA 245`;
+      expect(part2(input)).toBe(2868);
+    });
+    test('should return the total winnings for 2 hands where they share the same hand type, therefore cards need to be compared to determine rank', () => {
+      let input = `KK677 28
+              KTJJT 220`;
+      expect(part2(input)).toBe(468);
+
+      input = `T55J5 684
+            QQQJA 483`;
+      expect(part2(input)).toBe(1650);
+    });
+    test('should work with AOC Day 7 test input', () => {
+      let input = `32T3K 765
+        T55J5 684
+        KK677 28
+        KTJJT 220
+        QQQJA 483`;
+      expect(part2(input)).toBe(5905);
+    });
+    test('should work with AOC Day 7 main data input', () => {
+      expect(part2(mainDataInput)).toBe(251545216);
+    });
+  });
 });
---
+++ day7/day7-p2.js
@@ -0,0 +1,231 @@
+const part2 = (input) => {
+  if (input.length === 0) return 0;
+
+  const parsedInput = input
+    .split('\n')
+    .map((line) => line.trim())
+    .map((line) => line.split(' '));
+
+  const handsInfoObj = [];
+
+  for (let i = 0; i < parsedInput.length; i++) {
+    const { handType, typeRank } = convertHandToTypeP2(parsedInput[i][0]);
+
+    handsInfoObj.push({});
+
+    handsInfoObj[i].hand = parsedInput[i][0];
+    handsInfoObj[i].bid = Number(parsedInput[i][1]);
+    handsInfoObj[i].handConvToNum = convertHandToNumsP2(parsedInput[i][0]);
+    handsInfoObj[i].handType = handType;
+    handsInfoObj[i].handTypeRank = typeRank;
+    handsInfoObj[i].rank = 0;
+  }
+
+  let total = 0;
+
+  // Order the hands by the strength of the hand type ranking
+  const orderHandsByStrength = handsInfoObj.sort(
+    (a, b) => b.handTypeRank - a.handTypeRank
+  );
+
+  // 👇 a bit brute force, sorry! 👇
+
+  // Now go through the ranked types and check if any are the same, then conduct the card-by-card check
+  let handSortCount = 1000;
+
+  while (handSortCount !== 0) {
+    for (let i = 0; i < orderHandsByStrength.length; i++) {
+      if (orderHandsByStrength[i + 1] !== undefined) {
+        if (
+          orderHandsByStrength[i].handTypeRank ===
+          orderHandsByStrength[i + 1].handTypeRank
+        ) {
+          for (let j = 0; j < 5; j++) {
+            const currHandCard = orderHandsByStrength[i].handConvToNum[j];
+            const nextHandCard = orderHandsByStrength[i + 1].handConvToNum[j];
+
+            // Check if cards are not equal before comparing and swapping
+            if (currHandCard !== nextHandCard) {
+              if (currHandCard > nextHandCard) {
+                break;
+              } else if (nextHandCard > currHandCard) {
+                const currHandTmp = orderHandsByStrength[i];
+                const nextHandTmp = orderHandsByStrength[i + 1];
+                orderHandsByStrength[i] = nextHandTmp;
+                orderHandsByStrength[i + 1] = currHandTmp;
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+    handSortCount--;
+  }
+
+  orderHandsByStrength.forEach(
+    (hand, index) => (hand.rank = orderHandsByStrength.length - index)
+  );
+
+  // Now total up the ranks by multiplying the bid and the rank number, add to 'total'
+  orderHandsByStrength.forEach((hand) => (total += hand.bid * hand.rank));
+
+  return total;
+};
+
+const convertHandToTypeP2 = (hand) => {
+  let handType = '';
+  let typeRank = 0;
+
+  const typeRankLookup = {
+    '5aK': 7,
+    '4aK': 6,
+    Fh: 5,
+    '3aK': 4,
+    '2p': 3,
+    '1p': 2,
+    Hc: 1,
+  };
+
+  const handCardCount = {};
+
+  hand.split('').forEach((card) => {
+    if (!handCardCount.hasOwnProperty(card)) handCardCount[card] = 0;
+
+    handCardCount[card]++;
+  });
+
+  const cardNumsSorted = Object.values(handCardCount).sort((a, b) => b - a);
+
+  if (cardNumsSorted.length === 1) {
+    handType = `5aK`;
+    typeRank = typeRankLookup[handType];
+  } else if (cardNumsSorted.length === 2) {
+    if (cardNumsSorted[0] === 4 && cardNumsSorted[1] === 1) {
+      handType = `4aK`;
+    } else if (cardNumsSorted[0] === 3 && cardNumsSorted[1] === 2) {
+      handType = `Fh`;
+    }
+    typeRank = typeRankLookup[handType];
+  } else if (cardNumsSorted.length === 3) {
+    if (
+      cardNumsSorted[0] === 3 &&
+      cardNumsSorted[1] === 1 &&
+      cardNumsSorted[2] === 1
+    ) {
+      handType = `3aK`;
+    } else if (
+      cardNumsSorted[0] === 2 &&
+      cardNumsSorted[1] === 2 &&
+      cardNumsSorted[2] === 1
+    ) {
+      handType = `2p`;
+    }
+    typeRank = typeRankLookup[handType];
+  } else if (cardNumsSorted.length === 4) {
+    if (
+      cardNumsSorted[0] === 2 &&
+      cardNumsSorted[1] === 1 &&
+      cardNumsSorted[2] === 1 &&
+      cardNumsSorted[3] === 1
+    ) {
+      handType = `1p`;
+    }
+    typeRank = typeRankLookup[handType];
+  } else if (cardNumsSorted.length === 5) {
+    handType = `Hc`;
+    typeRank = typeRankLookup[handType];
+  }
+
+  // Now check the Joker cards and make amendment to hand if needed
+  const jokerCards = hand.split('').filter((card) => card === 'J').length;
+
+  if (handType === '4aK') {
+    if (jokerCards === 1) {
+      handType = '5aK';
+      typeRank = typeRankLookup[handType];
+    }
+  }
+
+  if (handType === 'Fh') {
+    if (jokerCards === 1) {
+      handType = '4aK';
+      typeRank = typeRankLookup[handType];
+    }
+    if (jokerCards === 2) {
+      handType = '5aK';
+      typeRank = typeRankLookup[handType];
+    }
+    if (jokerCards === 3) {
+      handType = '5aK';
+      typeRank = typeRankLookup[handType];
+    }
+  }
+
+  if (handType === '3aK') {
+    if (jokerCards === 1) {
+      handType = '4aK';
+      typeRank = typeRankLookup[handType];
+    }
+    if (jokerCards === 2) {
+      handType = '5aK';
+      typeRank = typeRankLookup[handType];
+    }
+  }
+
+  if (handType === '2p') {
+    if (jokerCards === 1) {
+      handType = 'Fh';
+      typeRank = typeRankLookup[handType];
+    }
+    if (jokerCards === 2) {
+      handType = '4aK';
+      typeRank = typeRankLookup[handType];
+    }
+    if (jokerCards === 3) {
+      handType = '5aK';
+      typeRank = typeRankLookup[handType];
+    }
+  }
+
+  if (handType === '1p') {
+    if (jokerCards === 1) {
+      handType = '3aK';
+      typeRank = typeRankLookup[handType];
+    }
+    if (jokerCards === 2) {
+      handType = '4aK';
+      typeRank = typeRankLookup[handType];
+    }
+    if (jokerCards === 3) {
+      handType = '5aK';
+      typeRank = typeRankLookup[handType];
+    }
+  }
+
+  return { handType, typeRank };
+};
+
+const convertHandToNumsP2 = (hand) => {
+  const handToNumLookup = {
+    A: 13,
+    K: 12,
+    Q: 11,
+    T: 10,
+    9: 9,
+    8: 8,
+    7: 7,
+    6: 6,
+    5: 5,
+    4: 4,
+    3: 3,
+    2: 2,
+    J: 1,
+  };
+
+  const handToNumArr = hand.split('').map((card) => handToNumLookup[card]);
+
+  return handToNumArr;
+};
+
+module.exports = { part2, convertHandToNumsP2, convertHandToTypeP2 };
